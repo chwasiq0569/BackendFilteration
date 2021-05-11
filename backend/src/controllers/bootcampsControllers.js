b80276ef3caf3bc87fbd598e00be2a69c80c2795
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 18:21:25 +0500

    Added Filteration & Sorting Functionality on Backend

--- backend/src/controllers/bootcampsControllers.js
+++ backend/src/controllers/bootcampsControllers.js
@@ -2,20 +2,21 @@ const asyncHandler = require("../middlewares/asyncHandler")
 const Bootcamp = require("../models/bootcamp")
 
 exports.getAllBootcamps = async (req, res, next) => {
-
+    let query;
     const reqQuery = { ...req.query }
-
     const removeFields = ["sort"]
-    
     removeFields.forEach(val => delete reqQuery[val])
-
-
     let queryStr = JSON.stringify(reqQuery)
-
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
-    
-    const bootcamps = await Bootcamp.find(JSON.parse(queryStr))
-
+    query = Bootcamp.find(JSON.parse(queryStr))
+    if(req.query.sort){
+        const sortbyArr = req.query.sort.split(',')
+        const sortByStr = sortbyArr.join(" ");
+        query = query.sort(sortByStr)
+    }else{
+        query = query.sort('-price')
+    }
+    const bootcamps = await query
     return res.status(200).json({
         success: true,
         data: bootcamps
@@ -43,11 +44,7 @@ exports.updateBootcampById = async (req, res, next) => {
 }
 
 exports.deleteBootcampById = async (req, res, next) => {
-    let bootcamp = await Bootcamp.findByIdAndRemove(req.params.id);
-    // if(!bootcamp){
-    //     return next(new ErrorResponse(`Bootcamp with ${req.params.id} not found`, 404))
-    // }
-    // await Bootcamp.remove();
+    await Bootcamp.findByIdAndRemove(req.params.id);
     return res.status(200).json({
         success: true,
         data: {}
