8f72190acd43ea8e5173c7373946b2cb0fe5d542
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 14:12:54 +0500

    Setup DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express();
+connectDB();
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
