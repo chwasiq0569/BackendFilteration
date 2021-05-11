d1bd6e2846fcde8f61860150d43f380b4612831d
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 17:05:18 +0500

    Added Create, Update, Delete, Retrieve endpoints

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = () => {
-     mongoose.connect(process.env.DB_CONNECTION_STRING,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true }).then(() => {
+     mongoose.connect(process.env.DB_CONNECTION_STRING,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
         console.log("DB Connection Established");   
      }).catch(() => {
         console.log("Connection Failed");   
--- backend/src/controllers/bootcampsControllers.js
+++ backend/src/controllers/bootcampsControllers.js
@@ -1,15 +1,42 @@
-exports.getAllBootcamps = (req, res, next) => {
-    res.send('GET ALL BOOTCAMPS ROUTE')
+const asyncHandler = require("../middlewares/asyncHandler")
+const Bootcamp = require("../models/bootcamp")
+
+exports.getAllBootcamps = async (req, res, next) => {
+    const bootcamps = await Bootcamp.find()
+    return res.status(200).json({
+        success: true,
+        data: bootcamps
+    })
 }
 
-exports.createNewBootcamp = (req, res, next) => {
-    res.send('CREATE NEW BOOTCAMP ROUTE')
+exports.createNewBootcamp = async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body);
+    return res.status(201).json({
+        success: true,
+        data: bootcamp
+    })
 }
 
-exports.updateBootcampById = (req, res, next) => {
-    res.send('UPDATE A BOOTCAMP BY AN ID ROUTE')
+exports.updateBootcampById = async (req, res, next) => {
+    let bootcamp = await Bootcamp.findById(req.params.id);
+    if(!bootcamp){
+        return next(new ErrorResponse(`Bootcamp with ${req.params.id} not found`, 404))
+    }
+    bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    return res.status(201).json({
+        success: true,
+        data: bootcamp
+    })
 }
 
-exports.deleteBootcampById = (req, res, next) => {
-    res.send('DELETE A BOOTCAMP BY AN ID ROUTE')
+exports.deleteBootcampById = async (req, res, next) => {
+    let bootcamp = await Bootcamp.findByIdAndRemove(req.params.id);
+    // if(!bootcamp){
+    //     return next(new ErrorResponse(`Bootcamp with ${req.params.id} not found`, 404))
+    // }
+    // await Bootcamp.remove();
+    return res.status(200).json({
+        success: true,
+        data: {}
+    })
 }
