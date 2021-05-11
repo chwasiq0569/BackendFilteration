6400c2ffa711946892d0060d879159c64d14f46c
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 14:16:21 +0500

    Setpup base route

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db')
 const app = express();
 connectDB();
 
+app.use('/api/v1', require('./routes/routes'))
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
