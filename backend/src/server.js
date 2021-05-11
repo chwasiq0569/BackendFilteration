265bc43c11ec8ab3012a55dadf9b9da844c94717
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-11 15:58:07 +0500

    wrapped controlelrs inside asyncHandler

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,14 @@ const connectDB = require('./config/db');
 const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
+
+app.use(express.json())
+
 connectDB();
 
 app.use('/api/v1/bootcamps', require('./routes/bootcampRoutes'))
 
-ap.use(errorHandler)
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server Running on PORT: ${PORT}`))
