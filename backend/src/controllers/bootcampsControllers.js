2de4c88dba13871103764123a99b236321c58215
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-05-12 06:36:22 +0500

    Finished Filtering & SOrting

--- backend/src/controllers/bootcampsControllers.js
+++ backend/src/controllers/bootcampsControllers.js
@@ -3,23 +3,56 @@ const Bootcamp = require("../models/bootcamp")
 
 exports.getAllBootcamps = async (req, res, next) => {
     let query;
+
+    let uiValues = {
+        filtering: {},
+        sorting: {}
+    }
+
     const reqQuery = { ...req.query }
     const removeFields = ["sort"]
     removeFields.forEach(val => delete reqQuery[val])
+
+    const filterKeys = Object.keys(reqQuery)
+    const filterValues= Object.values(reqQuery)
+
+    filterKeys.forEach((val, idx) => uiValues.filtering[val] = filterValues[idx])
+
     let queryStr = JSON.stringify(reqQuery)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
     query = Bootcamp.find(JSON.parse(queryStr))
     if(req.query.sort){
         const sortbyArr = req.query.sort.split(',')
+
+        sortbyArr.forEach(val => {
+            let order;
+
+            if(val[0] === '-'){
+                order = "descending"
+            }else{
+                order = "asscending"
+            }
+            uiValues.sorting[val.replace("-", "")] = order
+        })
+
         const sortByStr = sortbyArr.join(" ");
         query = query.sort(sortByStr)
     }else{
         query = query.sort('-price')
     }
     const bootcamps = await query
+
+    const maxPrice = await Bootcamp.find().sort({ price: -1 }).limit(1).select("-_id price");
+
+    const minPrice = await Bootcamp.find().sort({ price: 1 }).limit(1).select("-_id price");
+
+    uiValues.maxPrice = maxPrice[0].price
+    uiValues.minPrice = minPrice[0].price
+
     return res.status(200).json({
         success: true,
-        data: bootcamps
+        data: bootcamps,
+        uiValues
     })
 }
 
